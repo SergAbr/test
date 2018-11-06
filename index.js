a0c294823e2584847d91a67ffe0f0e41a5f63771
SerAbr <abrams.web@gmail.com>
2018-11-06 21:24:28 +0300

    reliz1

---
+++ index.js
@@ -0,0 +1,8 @@
+import jQuery from 'jquery';
+import bootstrap from 'bootstrap';
+import "./scss/main.scss";
+console.log ("HUI");
+
+jQuery(function() {
+    jQuery('body').css('color', 'blue');
+});
